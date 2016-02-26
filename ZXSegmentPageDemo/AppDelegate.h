85adf1afdffcacfe914a000cb06f3d39aa92c3f5
Shayne Chow <shayne@weplus.cn>
2016-02-26 15:46:45 +0800

    Initial Commit

---
+++ ZXSegmentPageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXSegmentPageDemo
+//
+//  Created by Xiang on 16/2/26.
+//  Copyright © 2016年 周想. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZXSegmentPageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXSegmentPageDemo
+//
+//  Created by Xiang on 16/2/26.
+//  Copyright © 2016年 周想. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
